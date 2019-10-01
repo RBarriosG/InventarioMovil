53786f8b672ffd1cc78c2433ffbf9b14d9dc49d7
rodolfo.barrrios@ceiba.com.co <rodolfo.barrios@ceiba.com.co>
2019-10-01 08:06:06 -0500

    Agregando Font-family : Roboto_medium, creando la clase y layout para el dialogo agregar articulos.

---
+++ app/src/main/java/com/malfor/inventariomovil/dialogo/DialogoAgregarArticulo.java
@@ -0,0 +1,55 @@
+package com.malfor.inventariomovil.dialogo;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.DialogFragment;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+import com.malfor.inventariomovil.R;
+
+public class DialogoAgregarArticulo extends DialogFragment {
+
+    public TextInputLayout tilCodigo;
+
+    public TextInputEditText tieCodigo;
+
+    public TextInputLayout tilDescripcion;
+
+    public TextInputEditText tieDescripcion;
+
+    public TextInputLayout tilPrecio;
+
+    public TextInputEditText tiePrecio;
+
+    public MaterialButton botonCancelar;
+
+    public MaterialButton botonGuardar;
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+
+        View dialogView = getActivity().getLayoutInflater().inflate(R.layout.dialogo_agregar_articulo, null);
+
+        builder.setView(dialogView);
+
+        tilCodigo = dialogView.findViewById(R.id.tilCodigo);
+        tieCodigo = dialogView.findViewById(R.id.tieCodigo);
+        tilDescripcion = dialogView.findViewById(R.id.tilDescripcion);
+        tieDescripcion = dialogView.findViewById(R.id.tieDescripcion);
+        tilPrecio = dialogView.findViewById(R.id.tilPrecio);
+        tiePrecio = dialogView.findViewById(R.id.tiePrecio);
+        botonCancelar = dialogView.findViewById(R.id.botonCancelar);
+        botonGuardar = dialogView.findViewById(R.id.botonGuardar);
+
+        return super.onCreateDialog(savedInstanceState);
+    }
+}
